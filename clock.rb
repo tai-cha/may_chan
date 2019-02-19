1d5168ba186e481c3cc593703f1e51d4ec342c68
taichanNE30 <ne300209@senshu-u.jp>
2019-02-20 02:46:03 +0900

    clockworkが仕事しないのでactive_supportを追加

--- clock.rb
+++ clock.rb
@@ -1,11 +1,11 @@
 require 'clockwork'
 require './bot.rb'
+require 'active_support/all'
 require 'time'
 include Clockwork
 
-every(1.minutes, 'upTweet.job') do
+every(1.minute, 'upTweet.job') do
     if Time.now.min % 5 == 0
-        puts "実行中..."
         upTweet
     end
 end
