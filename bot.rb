3e13bb21489914f38daf365c01c4407e6eb84ba2
taichanNE30 <ne300209@senshu-u.jp>
2019-02-20 01:01:44 +0900

    initial commit

---
+++ bot.rb
@@ -0,0 +1,67 @@
+require 'natto'
+require_relative './marcov.rb'
+require 'uri'
+require 'twitter'
+require 'cgi'
+
+@marcov = Marcov.new(text)
+@client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV['TWITTER_CK']
+    config.consumer_secret     = ENV['TWITTER_CS']
+    config.access_token        = ENV['TWITTER_TOKEN']
+    config.access_token_secret = ENV['TWITTER_SECRET']
+end
+
+def removeURL(text)
+    URI.extract(text).uniq.each {|url| text.gsub!(url, '')}
+    return text
+end
+
+def removeMention(text)
+    return text.gsub(/@[^\s\R]+[\s\R$]*|@[^\s]*$/,"")
+end
+
+def removeHashTags(text)
+    return text.gsub(/#[^\s\R]+[\s\R$]*|#[^\s]*$/,"")
+end
+
+def removeRT(text)
+    return text.gsub(/^RT/,"")
+end
+
+def makeText(texts)
+    threeWordsArray = []
+    texts.each do |text|
+        text = removeURL(removeHashTags(removeMention(removeRT(text))))
+        @marcov.makeThreeWordsArray(@marcov.makeWordsArray(text)).each do |threeWords|
+            threeWordsArray.push threeWords
+        end
+    end
+    return @marcov.makeSentence(threeWordsArray)
+end
+
+def makeTextArray(tweets)
+    texts = []
+    tweets.each do |tweet|
+        texts.push CGI.unescapeHTML(removeMention(tweet.text))
+    end
+    return texts
+end
+
+def upTweet
+    tweets = @client.home_timeline(count: 200)
+    text=''
+    while(text.length <= 5 || text.length >= 140)
+        text = makeText(makeTextArray(tweets))
+    end
+    @client.update(text, options = {})
+    puts text
+end
+
+# For debug
+# tweets = @client.home_timeline(count: 200)
+# 10.times do
+#     puts makeText(makeTextArray(tweets))
+# end
+
+upTweet
---
+++ marcov.rb
@@ -0,0 +1,60 @@
+class Marcov
+
+    def initialize(text)
+        @text = text
+        @nm = Natto::MeCab.new
+    end
+
+    def makeWordsArray(text)
+        words = []
+        @nm.parse(text) do |n|
+            words.push "#{n.surface}"
+        end
+        words -= [""]
+        return words
+    end
+
+    def makeThreeWordsArray(words)
+        threeWords = []
+        words.unshift(nil)
+        words.push(nil)
+        (0..words.length-3).each do |i|
+            threeWords.push [words[i], words[i+1], words[i+2]]
+        end
+        return threeWords
+    end
+
+    def findAndSelectBlock(threeWordsArray, target)
+        options = []
+        # puts "targetは#{[target].to_s}です"
+        threeWordsArray.each do |threeWords|
+            options.push threeWords if threeWords[0] == target
+        end
+        random = Random.new()
+        # puts options.to_s.gsub("\n","\t")
+        return [nil,nil,nil] if options.length <= 0
+        return options[random.rand(0..(options.length - 1))]
+    end
+
+    def makeSentence(threeWords)
+        # puts threeWords.to_s
+        threeBlocks = []
+        result = ""
+        threeBlocks.push findAndSelectBlock(threeWords, nil)
+        while threeBlocks.last.last != nil do
+            threeBlocks.push findAndSelectBlock(threeWords, threeBlocks.last.last)
+        end
+        # puts threeBlocks.to_s
+        threeBlocks.each do |block|
+            for i in (1..2) do
+                result << block[i] unless block[i].nil?
+            end
+        end
+        return result
+    end
+
+    def result
+        return makeSentence(makeThreeWordsArray(makeWordsArray(@text)))
+    end
+
+end
